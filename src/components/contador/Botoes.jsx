ec9b52fea64b828e05202ca3060531f3daf35853
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-04-03 08:44:06 -0300

    feat: accountant

---
+++ src/components/contador/Botoes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) => {
+
+    return (<div>
+
+        <button onClick={props.setDec}>-</button>
+        <button onClick={props.setInc}>+</button>
+
+    </div>
+    )
+
+}
--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
+import Display from "./Display"
+import Botoes from './Botoes'
+import PassoForm from './PassoForm'
 
 export default class Contador extends Component {
 
     state = {
 
-        numero: this.props.numeroInicial,
+        numero: this.props.numeroInicial || 0,
+        passo: this.props.numeroInicial || 10,
 
 
     }
 
-    inc() {
+    inc = () => {
 
         this.setState({
 
-            numero: this.state.numero + 1
+            numero: this.state.numero + this.state.passo
+
+        })
+
+    }
+
+
+    dec = () => {
+
+        this.setState({
+
+            numero: this.state.numero - this.state.passo
+
+        })
+
+    }
+
+
+    setPasso = (novoPasso) => {
+
+        this.setState({
+
+            passo: novoPasso,
 
         })
 
@@ -26,10 +52,9 @@ export default class Contador extends Component {
             <div>
 
                 <h2>Contador</h2>
-               {/* <p>{ this.props.numeroInicial }</p>*/}
-               <h3>{this.state.numero}</h3>
-                <button onClick={this.inc}>+</button>
-
+                <Display numero = {this.state.numero} />
+                <PassoForm passo = {this.state.passo } setPasso = {this.setPasso}/>
+             <Botoes setInc = {this.inc} setDec = {this.dec}/>
             </div>
 
         )
